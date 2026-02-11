577514ce0d07f4c5f1b9086bc2449813cae84544
leekyuhyun <leekh010502@gmail.com>
2026-02-11 15:25:52 +0900

    feat : app.js 기본 구현

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import userRouter from "./routes/users.js";
+import bookRouter from "./routes/books.js";
+import likeRouter from "./routes/likes.js";
+import cartRouter from "./routes/carts.js";
+import orderRouter from "./routes/orders.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+app.use("/users", userRouter);
+app.use("/books", bookRouter);
+app.use("/likes", likeRouter);
+app.use("/carts", cartRouter);
+app.use("/orders", orderRouter);
+
+app.listen(PORT, () => {
+  console.log(`${PORT}번 포트에서 서버가 실행중입니다.`);
+});
