155aec97966da16660da3ea4692d9644490bb6fe
leekyuhyun <leekh010502@gmail.com>
2026-02-12 16:25:31 +0900

    feat : users api 구현

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,64 @@
 import express from "express";
+import { body } from "express-validator";
+import { validator } from "../middleware/validator.js";
+import {
+  join,
+  login,
+  pwdResetReq,
+  pwdReset,
+} from "../controller/userController.js";
 
 const router = express.Router();
 
 router
-  .post("/join", (req, res) => {
-    res.json("회원가입");
-  })
+  .post(
+    "/join",
+    [
+      body("email").notEmpty().isEmail().withMessage("이메일을 확인해주세요."),
+      body("name").notEmpty().isString().withMessage("이름을 확인해주세요."),
+      body("password")
+        .notEmpty()
+        .isString()
+        .withMessage("비밀번호를 확인해주세요."),
+    ],
+    validator,
+    join,
+  )
 
-  .post("/login", (req, res) => {
-    res.json("로그인");
-  })
+  .post(
+    "/login",
+    [
+      body("email")
+        .notEmpty()
+        .isEmail()
+        .withMessage("올바른 이메일 형식이 아닙니다."),
+      body("password")
+        .notEmpty()
+        .isString()
+        .withMessage("비밀번호를 입력해주세요."),
+    ],
+    validator,
+    login,
+  )
 
-  .post("/reset", (req, res) => {
-    res.json("비밀번호 초기화 요청");
-  })
+  .post(
+    "/reset",
+    [body("email").notEmpty().isEmail().withMessage("이메일을 입력해주세요.")],
+    validator,
+    pwdResetReq,
+  )
 
-  .put("/reset", (req, res) => {
-    res.json("비밀번호 초기화");
-  });
+  .put(
+    "/reset",
+    [
+      body("email").notEmpty().isEmail().withMessage("이메일을 입력해주세요."),
+      body("password")
+        .notEmpty()
+        .isString()
+        .withMessage("새로운 비밀번호를 입력해주세요."),
+    ],
+    validator,
+    pwdReset,
+  );
 
 export default router;
