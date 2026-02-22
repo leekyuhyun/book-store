c5f400833d338e042129b3c6a83ad08bba70d7f4
leekyuhyun <leekh010502@gmail.com>
2026-02-22 17:22:08 +0900

    fix : 서브쿼리, AS 추가, 페이징 코드 수정

--- controller/bookController.js
+++ controller/bookController.js
@@ -5,30 +5,34 @@ import { StatusCodes } from "http-status-codes";
 export const getAllbooks = (req, res) => {
   let { category_id, news, limit, currentPage } = req.query;
 
-  let parsedLimit = parseInt(limit);
-  let offset = (parseInt(currentPage) - 1) * parsedLimit;
-
-  let sql = "SELECT * FROM books";
-  let values = [parseInt(limit), offset];
+  let sql = `SELECT *, 
+                (SELECT count(*) FROM likes WHERE liked_book_id = books.book_id) AS likes 
+             FROM books`;
+  let values = [];
 
   if (category_id && news) {
     sql +=
       " WHERE category_id = ? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()";
-    values = [category_id, news];
+    values.push(category_id);
   } else if (category_id) {
     sql += " WHERE category_id = ?";
-    values = [category_id];
+    values.push(category_id);
   } else if (news) {
     sql +=
       " WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()";
   }
 
-  sql += " LIMIT ? OFFSET ?";
-  values.push(parsedLimit, offset);
+  if (limit && currentPage) {
+    let parsedLimit = parseInt(limit);
+    let offset = (parseInt(currentPage) - 1) * parsedLimit;
+
+    sql += " LIMIT ? OFFSET ?";
+    values.push(parsedLimit, offset);
+  }
 
   conn.query(sql, values, (err, results) => {
     if (err) {
-      console.error("도서 전체 조회 요청 DB 에러:", err);
+      console.error("도서 조회 DB 에러:", err);
       return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err);
     }
     return res.status(StatusCodes.OK).json(results);
@@ -37,7 +41,12 @@ export const getAllbooks = (req, res) => {
 
 export const getBookById = (req, res) => {
   const { id } = req.params;
-  const sql = `SELECT * FROM books LEFT JOIN category ON books.category_id = category.category_id WHERE books.book_id = ?`;
+  const sql = `SELECT books.*, category.name AS category_name,
+                  (SELECT count(*) FROM likes WHERE liked_book_id = books.book_id) AS likes 
+               FROM books 
+               LEFT JOIN category ON books.category_id = category.category_id 
+               WHERE books.book_id = ?`;
+
   conn.query(sql, [id], (err, results) => {
     if (err) {
       console.error("도서 개별 조회 요청 DB 에러:", err);
