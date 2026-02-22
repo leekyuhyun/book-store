511cec5735975cf024c1c874107db128c8b2e1b5
leekyuhyun <leekh010502@gmail.com>
2026-02-22 17:20:55 +0900

    feat & fix : sql문 분리

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -1,4 +1,3 @@
--- 유저 테이블 시작--
 -- 유저 테이블 생성 --
 CREATE TABLE users (
   id INT NOT NULL AUTO_INCREMENT,
@@ -8,11 +7,7 @@ CREATE TABLE users (
   PRIMARY KEY (`id`),
   UNIQUE INDEX `email_UNIQUE` (`email` ASC)
 );
--- users id 컬럼명 변경 --
-ALTER TABLE users CHANGE COLUMN id user_id INT NOT NULL AUTO_INCREMENT;
--- 유저 테이블 끝--
 
--- 도서 테이블 시작 --
 -- 도서 테이블 생성 --
 CREATE TABLE `books` (
   id INT NOT NULL AUTO_INCREMENT,
@@ -29,35 +24,31 @@ CREATE TABLE `books` (
   PRIMARY KEY (`id`),
   UNIQUE INDEX `isbn_UNIQUE` (`isbn` ASC)
 );
---  books id 컬럼명 변경 --
-ALTER TABLE books CHANGE COLUMN id book_id INT NOT NULL AUTO_INCREMENT;
--- 예약어로 인한 컬럼명 수정 --
-ALTER TABLE books CHANGE COLUMN `format` form VARCHAR(45) NOT NULL;
-ALTER TABLE books CHANGE COLUMN `description` detail LONGTEXT NULL;
-ALTER TABLE books CHANGE COLUMN `index` contents LONGTEXT NULL;
--- 컬럼 추가--
-ALTER TABLE books ADD COLUMN img INT NULL AFTER title;
-ALTER TABLE books ADD COLUMN category_id INT NULL AFTER img;
--- categoty_id 컬럼 수정 --
-ALTER TABLE books ADD INDEX category_id_idx (`category_id` ASC);
-ALTER TABLE books ADD CONSTRAINT category_id FOREIGN KEY (category_id) REFERENCES category (id);
--- pub_date 수정 --
-UPDATE books SET pub_date = '2026-02-15' WHERE book_id = 11;
-UPDATE books SET pub_date = '2026-02-05' WHERE book_id = 10;
-UPDATE books SET pub_date = '2026-01-25' WHERE book_id = 9;
-UPDATE books SET pub_date = '2026-01-19' WHERE book_id = 8;
-UPDATE books SET pub_date = '2025-11-20' WHERE book_id = 7;
-UPDATE books SET pub_date = '2025-02-10' WHERE book_id = 6;
-UPDATE books SET pub_date = '2024-06-15' WHERE book_id = 5;
 
--- 도서 테이블 끝 --
-
--- 카테고리 테이블 시작 --
 -- 카테고리 테이블 생성 --
 CREATE TABLE category (
   id INT NOT NULL,
   name VARCHAR(100) NOT NULL,
   PRIMARY KEY(id)
 );
--- id 컬럼명 변경 --
-ALTER TABLE category CHANGE COLUMN id category_id INT NOT NULL;
+
+-- 좋아요 테이블 생성 --
+CREATE TABLE likes (
+  user_id INT NOT NULL,
+  liked_book_id INT NOT NULL,
+
+  INDEX user_id_idx (`user_id` ASC),
+  INDEX liked_book_id_idx (`liked_book_id` ASC),
+
+  CONSTRAINT fk_likes_user
+    FOREIGN KEY (`user_id`)
+    REFERENCES users (`user_id`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION,
+
+  CONSTRAINT `fk_likes_book`
+    FOREIGN KEY (`liked_book_id`)
+    REFERENCES books (`book_id`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION
+)
